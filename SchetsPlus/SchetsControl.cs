1361d2862527775ef4389ca2915eb32ac1e98bd3
William <wkos@live.nl>
2012-11-07 13:22:20 +0100

    comments gezet begin

--- SchetsPlus/SchetsControl.cs
+++ SchetsPlus/SchetsControl.cs
@@ -7,57 +7,57 @@ namespace SchetsPlus
 {
     public class SchetsControl : System.Windows.Forms.UserControl
     {
-        private Bitmap overlayBitmap;
+        private Bitmap overlayBitmap;   //Declareren overlayBitmap die later gebruikt wordt
 
-        public ISchetsTool currentTool;
-        public Action currentAction;
+        public ISchetsTool currentTool; //Declareren van de currenttool die gebruikt wordt
+        public Action currentAction;    //Declareren van de currentAction die gebruikt wordt
 
-        private Boolean muisVast;
-        public Schets schets;
-        public int lineThickness = 3;
+        private Boolean muisVast;   //Declareren boolean muisVast die checkt of muis ingedrukt is
+        public Schets schets;       //Declareren van schets
+        public int lineThickness = 3;   //Declareren en zetten van de lijndikte op 3
 
         public SchetsControl(string imageName)
         {
-            schets = new Schets(imageName, new Size(700, 500));
+            schets = new Schets(imageName, new Size(700, 500)); //Aanmaken van nieuw schetsobject
 
-            currentTool = App.availableTools[0];
-            currentAction = new PenAction();
+            currentTool = App.availableTools[0];    //Zeggen dat currentTool een pen is (standaard bij openen)
+            currentAction = new PenAction();    //Ook gelijk de penAction hierbij aanroepen
             
-            this.DoubleBuffered = true;
+            this.DoubleBuffered = true; //Voorkomen van flikkeren van scherm tijdens tekenen
 
             this.Paint += this.teken;
             this.Resize += this.veranderAfmeting;
 
             this.MouseDown += (object o, MouseEventArgs mea) =>
             {
-                muisVast = true;
+                muisVast = true;    //Zeggen dat muisVast true is, zodra je muis indrukt
 
-                overlayBitmap = new Bitmap(schets.imageSize.Width, schets.imageSize.Height);
-                setAction();
+                overlayBitmap = new Bitmap(schets.imageSize.Width, schets.imageSize.Height);    //Zetten van overlayBitmap met de width en height
+                setAction();    
 
-                Point translatedPoint = translateMouseCoordinates(mea.Location);
+                Point translatedPoint = translateMouseCoordinates(mea.Location);    //Pak de muis-coördinaten
 
-                currentTool.MuisVast(this, translatedPoint);
+                currentTool.MuisVast(this, translatedPoint);    //Gebruiken muiscoördinaten bij aanroepen methode muisVast
                 currentAction.onMouseDown(this, translatedPoint.X, translatedPoint.Y, 3, schets.primaryColor);
             };
             this.MouseMove += (object o, MouseEventArgs mea) =>
             {
-                if (muisVast)
+                if (muisVast)   //Doe dit alleen indien muis is ingedrukt
                 {
-                    Point translatedPoint = translateMouseCoordinates(mea.Location);
+                    Point translatedPoint = translateMouseCoordinates(mea.Location);    //Houdt telkens bij welke punten je over gaat
 
-                    currentTool.MuisDrag(this, translatedPoint);
+                    currentTool.MuisDrag(this, translatedPoint);    //Aanroepen methode muisdrag met deze muiscoördinaten
                     currentAction.onMouseMove(translatedPoint.X, translatedPoint.Y);
                 }
             };
             this.MouseUp += (object o, MouseEventArgs mea) =>
             {
-                muisVast = false;
+                muisVast = false;   //Boolean muisVast op false zetten aangezien muis nu los is
 
-                Point translatedPoint = translateMouseCoordinates(mea.Location);
+                Point translatedPoint = translateMouseCoordinates(mea.Location);    //Bekijken op welke coördinaten dit gebeurde
 
-                currentTool.MuisLos(this, translatedPoint);
-                if (currentAction != null)
+                currentTool.MuisLos(this, translatedPoint);     //Aanroepen methode muislos met deze coördinaten
+                if (currentAction != null)  //Indien er een action was gebruikt voeg dit dan toe aan de action list en update de historyBox
                 {
                     currentAction.onMouseUp(translatedPoint.X, translatedPoint.Y);
                     App.currentSchetsWindow.currentSchetsControl.schets.actions.Add(App.currentSchetsWindow.currentSchetsControl.currentAction);
@@ -71,7 +71,7 @@ namespace SchetsPlus
                     currentTool.Letter(this, kpea.KeyChar);
                     if (currentAction is TextAction)
                     {
-                        ((TextAction)currentAction).enteredText += kpea.KeyChar;
+                        ((TextAction)currentAction).enteredText += kpea.KeyChar;    //Voeg de letter toe aan de enteredTExt indien het textAction was
                     }
                 }
             };
@@ -91,13 +91,13 @@ namespace SchetsPlus
             this.Invalidate();
         }
 
-        public Graphics MaakBitmapGraphics()
+        public Graphics MaakBitmapGraphics()    //Maak nieuwe bitmapgraphics aan
         {   
-            Graphics g = schets.BitmapGraphics;
+            Graphics g = schets.BitmapGraphics;     
             g.SmoothingMode = SmoothingMode.AntiAlias;
             return g;
         }
-        public Graphics MaakOverlayBitmapGraphics()
+        public Graphics MaakOverlayBitmapGraphics() //Maak nieuwe overlaybitmap graphics aan
         {
             if (overlayBitmap == null)
             {
@@ -119,7 +119,7 @@ namespace SchetsPlus
             this.veranderAfmeting(o, ea);
         }
         
-        public Point translateMouseCoordinates(Point mouseLocation)
+        public Point translateMouseCoordinates(Point mouseLocation) //Indien het scherm verkleind is, verander dan de muiscoördinaten
         {
             double widthRatio = (double) schets.imageSize.Width / this.Width;
             double heightRatio = (double)schets.imageSize.Height / this.Height;
@@ -130,7 +130,7 @@ namespace SchetsPlus
             return new Point(newMouseX, newMouseY);
         }
 
-        private void setAction()
+        private void setAction()        //Het zetten van de acties bij de verschillende tools
         {
             if (currentTool is TekstTool)
             {
@@ -169,7 +169,7 @@ namespace SchetsPlus
                 currentAction = new FancyEraserAction();
             }
 
-            currentAction.actionColor = schets.primaryColor;
+            currentAction.actionColor = schets.primaryColor;        //Zeggen welke kleur er gebruikt moet worden
         }
     }
 }
--- SchetsPlus/Tools.cs
+++ SchetsPlus/Tools.cs
@@ -8,66 +8,66 @@ namespace SchetsPlus
 {
     public interface ISchetsTool
     {
-        void MuisVast(SchetsControl s, Point p);
+        void MuisVast(SchetsControl s, Point p);    //Zeggen dat deze methodes gebruikt zullen gaan worden
         void MuisDrag(SchetsControl s, Point p);
         void MuisLos(SchetsControl s, Point p);
         void Letter(SchetsControl s, char c);
     }
 
-    public abstract class StartpuntTool : ISchetsTool
+    public abstract class StartpuntTool : ISchetsTool   
     {
-        protected Point startpunt;
+        protected Point startpunt;  //Aanmaken van het startpunt en van de kleur waarmee getekent wordt
         protected Brush kwast;
 
         public virtual void MuisVast(SchetsControl s, Point p)
         {
-            kwast = new SolidBrush(s.schets.primaryColor);
-            startpunt = p;
+            kwast = new SolidBrush(s.schets.primaryColor);  //Zeggen welke kleur de kwast is
+            startpunt = p;  //Zeggen wat het startpunt is, dus het punt waarop muis ingedrukt wordt
         }
-        public virtual void MuisLos(SchetsControl s, Point p){}
+        public virtual void MuisLos(SchetsControl s, Point p){}     //Virtuele en abstracte methodes die later gebruikt worden
         public abstract void MuisDrag(SchetsControl s, Point p);
         public abstract void Letter(SchetsControl s, char c);
     }
 
     public class TekstTool : StartpuntTool
     {
-        string enteredText;
+        string enteredText; //Declareren van de ingevoerde tekst
 
         public override void MuisVast(SchetsControl s, Point p)
         {
-            base.MuisVast(s, p);
-            enteredText = "";
+            base.MuisVast(s, p);    //Aanroepen van de muisvastmethode van startpunttool
+            enteredText = "";   //Zetten dat ingevoerde tekst standaard niks is
         }
-        public override void MuisDrag(SchetsControl s, Point p)
+        public override void MuisDrag(SchetsControl s, Point p) //Bij tekst is er geen muisdrag
         {
         }
 
         public override void Letter(SchetsControl s, char c)
         {
-            enteredText += c;
+            enteredText += c;   //Zeggen dat de ingevoerde tekst een char erbij krijgt
 
-            Graphics g = s.MaakBitmapGraphics();
+            Graphics g = s.MaakBitmapGraphics();    //Aanmaken graphics voor het tekenen
 
-            Font font = new Font("Tahoma", 40);
-            string tekst = c.ToString();
-            SizeF sz = g.MeasureString(enteredText, font, this.startpunt, StringFormat.GenericTypographic);
-            g.DrawString(enteredText, font, kwast, this.startpunt, StringFormat.GenericTypographic);
+            Font font = new Font("Tahoma", 40);     //Zeggen welk lettertyp en grootte
+            string tekst = c.ToString();            
+            SizeF sz = g.MeasureString(enteredText, font, this.startpunt, StringFormat.GenericTypographic); //De grootte berekenen
+            g.DrawString(enteredText, font, kwast, this.startpunt, StringFormat.GenericTypographic);    //De string tekenen op het scherm
 
-            s.currentAction.endPoint[0] = (int) (s.currentAction.startPoint[0] + sz.Width);
-            s.currentAction.endPoint[1] = (int) (s.currentAction.startPoint[1] + sz.Height);
-            s.Invalidate();
+            s.currentAction.endPoint[0] = (int) (s.currentAction.startPoint[0] + sz.Width);     //De endpoint.X variabel maken aan de hoeveelheid ingevulde tekst
+            s.currentAction.endPoint[1] = (int)(s.currentAction.startPoint[1] + sz.Height);     //De endpoint.Y variabel maken aan de hoeveelheid ingevulde tekst
+            s.Invalidate(); //Teken het scherm opnieuw 
         }
     }
 
     public abstract class TweepuntTool : StartpuntTool
     {
-        public static Rectangle maakRechthoek(Point p1, Point p2)
+        public static Rectangle maakRechthoek(Point p1, Point p2)   //Methode die ervoor zorgt dat de rectangle gemaakt wordt
         {   
             return new Rectangle( new Point(Math.Min(p1.X,p2.X), Math.Min(p1.Y,p2.Y)), new Size (Math.Abs(p1.X-p2.X), Math.Abs(p1.Y-p2.Y)));
         }
         public static Pen MaakPen(Brush b, int dikte)
         {   
-            Pen pen = new Pen(b, dikte);
+            Pen pen = new Pen(b, dikte);    //Aanmaken van de pen
             pen.StartCap = LineCap.Round;
             pen.EndCap = LineCap.Round;
             return pen;
@@ -75,43 +75,43 @@ namespace SchetsPlus
 
         public override void MuisVast(SchetsControl s, Point p)
         {  
-            base.MuisVast(s, p);
+            base.MuisVast(s, p);    //Gebruik de methode muisvast van startpunttool
         }
         public override void MuisDrag(SchetsControl s, Point p)
         {
-            base.MuisLos(s, p);
+            base.MuisLos(s, p);     //Gebruik de methode muislos van startpunttool
 
-            s.MaakOverlayBitmapGraphics().Clear(Color.Transparent);
-            this.Bezig(s.MaakOverlayBitmapGraphics(), this.startpunt, p);
-            s.Invalidate();
+            s.MaakOverlayBitmapGraphics().Clear(Color.Transparent);     
+            this.Bezig(s.MaakOverlayBitmapGraphics(), this.startpunt, p);   //Roep de methode bezig aan
+            s.Invalidate();     //Teken het scherm opnieuw
         }
         public override void MuisLos(SchetsControl s, Point p)
         {   
-            base.MuisLos(s, p);
+            base.MuisLos(s, p); //Gebruik de methode muislos van startpunttool
 
-            s.MaakOverlayBitmapGraphics().Clear(Color.Transparent);
-            this.Bezig(s.MaakBitmapGraphics(), startpunt, p);
-            s.Invalidate();
+            s.MaakOverlayBitmapGraphics().Clear(Color.Transparent); 
+            this.Bezig(s.MaakBitmapGraphics(), startpunt, p);   //Roep de methode bezig aan
+            s.Invalidate(); //Teken het scherm opnieuw
         }
-        public override void Letter(SchetsControl s, char c)
+        public override void Letter(SchetsControl s, char c)    //Moet overriden omdat letter abstract is
         {
         }
 
-        public abstract void Bezig(Graphics g, Point p1, Point p2);
+        public abstract void Bezig(Graphics g, Point p1, Point p2);     //Aanmaken abstracte methode bezig
     }
 
     public class RechthoekTool : TweepuntTool
     {
-        public override void Bezig(Graphics g, Point p1, Point p2)
+        public override void Bezig(Graphics g, Point p1, Point p2)  
         {   
-            g.DrawRectangle(MaakPen(kwast, 3), TweepuntTool.maakRechthoek(p1, p2));
+            g.DrawRectangle(MaakPen(kwast, 3), TweepuntTool.maakRechthoek(p1, p2)); //Overriden methode bezig en teken een rectangle met behulp van tweepunttool.maakrechthoek
         }
     }
     public class VolRechthoekTool : RechthoekTool
     {
         public override void Bezig(Graphics g, Point p1, Point p2)
         {
-            g.FillRectangle(kwast, TweepuntTool.maakRechthoek(p1, p2));
+            g.FillRectangle(kwast, TweepuntTool.maakRechthoek(p1, p2)); //Overriden methode bezig en teken een gevulde rectangle met behulp van tweepunttool.maaktrechthoek
         }
     }
 
@@ -119,19 +119,19 @@ namespace SchetsPlus
     {
         public override void Bezig(Graphics g, Point p1, Point p2)
         {
-            g.DrawEllipse(MaakPen(kwast, 3), TweepuntTool.maakRechthoek(p1, p2));
+            g.DrawEllipse(MaakPen(kwast, 3), TweepuntTool.maakRechthoek(p1, p2));   //Overriden methode bezig en teken een ellipse met behulp van tweepunttool.maakrechthoek
         }
     }
     public class FillEllipseTool : TweepuntTool
     {
         public override void MuisVast(SchetsControl s, Point p)
         {
-            base.MuisVast(s, p);
+            base.MuisVast(s, p);    //Gebruik de methode muisvast uit tweepunttool
         }
 
         public override void Bezig(Graphics g, Point p1, Point p2)
         {
-            g.FillEllipse(kwast, TweepuntTool.maakRechthoek(p1, p2));
+            g.FillEllipse(kwast, TweepuntTool.maakRechthoek(p1, p2));   //Overriden methode bezig en teken een gevulde ellipse met behulp van tweepunttool.maakrechthoek
         }
     }
 
@@ -140,16 +140,16 @@ namespace SchetsPlus
     {
         public override void Bezig(Graphics g, Point p1, Point p2)
         {
-            g.DrawLine(MaakPen( kwast,3), p1.X, p1.Y, p2.X, p2.Y);
+            g.DrawLine(MaakPen( kwast,3), p1.X, p1.Y, p2.X, p2.Y);  //Overriden methode bezig en teken een lijn tussen de begin- en eindpunten
         }
     }
 
     public class PenTool : LijnTool
     {
-        public override void MuisDrag(SchetsControl s, Point p)
+        public override void MuisDrag(SchetsControl s, Point p) //Teken kleine lijntjes tussen de losse punten van de pen
         {
-            this.MuisLos(s, p);
-            this.MuisVast(s, p);
+            this.MuisLos(s, p); //Gebruik de methode muislos van lijntool -> dus van tweepunttool
+            this.MuisVast(s, p);    //Gebruik de methode muisvast van lijntool -> dus van tweepunttool
         }
     }
     
@@ -157,7 +157,7 @@ namespace SchetsPlus
     {
         public override void Bezig(Graphics g, Point p1, Point p2)
         {   
-            g.DrawLine(MaakPen(Brushes.White, 3), p1.X, p1.Y, p2.X, p2.Y);
+            g.DrawLine(MaakPen(Brushes.White, 3), p1.X, p1.Y, p2.X, p2.Y); //Overriden methode bezig en teken een witte lijn tussen de begin- en eindpunten van de lijn
         }
     }
 
@@ -165,39 +165,39 @@ namespace SchetsPlus
     {
         public override void MuisLos(SchetsControl s, Point p)
         {
-            base.MuisLos(s, p);
-            Boolean hasErased = false;
-            int i;
+            base.MuisLos(s, p); //Gebruik de methode muislos van startpunttool
+            Boolean hasErased = false;  //Zet de bool hasErased op false zodat er nog niet gegumt is
+            int i;  //Declareer int i voor een latere for-loop
             if(s.schets.actionEraseLimit == -1)
             {
-                i = s.schets.actions.Count - 1;
+                i = s.schets.actions.Count - 1; //Zetten van int i
             }
             else
             {
-                i = s.schets.actionEraseLimit;
+                i = s.schets.actionEraseLimit;  //Zetten van int i
             }
             for (; i >= 0; i--)
             {
-                if (!(s.schets.actions[i] is FancyEraserAction) && s.schets.actions[i].isInClick(p.X, p.Y))
+                if (!(s.schets.actions[i] is FancyEraserAction) && s.schets.actions[i].isInClick(p.X, p.Y)) //Zeggen wat er moet gebeuren als je ergens op klikt en dit geen fancyEraser object is
                 {
-                    if (!s.schets.actions[i].drawAction)
+                    if (!s.schets.actions[i].drawAction)    //Kijken of het een tekenactie is of niet, indien niet dan continue
                     {
                         continue;
                     }
-                    s.schets.actions[i].drawAction = false;
-                    ((FancyEraserAction)s.currentAction).erasedAction = s.schets.actions[i];
-                    hasErased = true;
-                    break;
+                    s.schets.actions[i].drawAction = false; //Zet drawaction op false
+                    ((FancyEraserAction)s.currentAction).erasedAction = s.schets.actions[i];    //Gum deze action weg
+                    hasErased = true;   //Zet hasErased op true, dus er is gegumt
+                    break;  //Break uit de loop
                 }
             }
-            if (!hasErased)
+            if (!hasErased) //Als hasErased nog valt is, dan zet currentAction = null
             {
                 s.currentAction = null;
             }
 
         }
 
-        public override void MuisDrag(SchetsControl s, Point p) { }
+        public override void MuisDrag(SchetsControl s, Point p) { } //Overriden methodes muisdrag en letter aangezien ze abstract zijn, maar zet niks in body
         public override void Letter(SchetsControl s, char c)  { }
     }
 }
